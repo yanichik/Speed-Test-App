48463e9409741411155965cb62d6206fed8a2a30
Yan Brunshteyn <brunshte@gmail.com>
2024-06-18 16:58:58 -0700

    fully zoomed in cluster to display sheet up alert of results inside cluster on click

--- SpeedTest/Annotations/CustomTestResultClusterView.swift
+++ SpeedTest/Annotations/CustomTestResultClusterView.swift
@@ -10,13 +10,32 @@ import MapKit
 
 class CustomTestResultClusterView: MKAnnotationView {
     @IBOutlet private weak var countLabel: UILabel!
+    var memberAnnotations: [any MKAnnotation]?
+    var coordinate: CLLocationCoordinate2D?
+    var title: String?
+    
     
     override var annotation: MKAnnotation? {
         didSet {
             guard let annotation = annotation as? MKClusterAnnotation else {
                 return
             }
+            memberAnnotations = annotation.memberAnnotations
+            coordinate = annotation.coordinate
+            title = annotation.title
             clusteringIdentifier = annotation.title
+            let myView = UILabel()
+            myView.backgroundColor = .green
+            myView.text = "Click Here"
+            myView.adjustsFontSizeToFitWidth = true
+            
+            let widthConstraint = NSLayoutConstraint(item: myView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 80)
+            myView.addConstraint(widthConstraint)
+            
+            let heightConstraint = NSLayoutConstraint(item: myView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 20)
+            myView.addConstraint(heightConstraint)
+            
+            detailCalloutAccessoryView = myView
 //            detailCalloutAccessoryView = CustomCalloutView(annotation: annotation.memberAnnotations[0] as! CustomPointAnnotation)
 //            leftCalloutAccessoryView = CustomCalloutView(annotation: annotation.memberAnnotations[1] as! CustomPointAnnotation)
 //            rightCalloutAccessoryView = CustomCalloutView(annotation: annotation.memberAnnotations[2] as! CustomPointAnnotation)
@@ -27,7 +46,7 @@ class CustomTestResultClusterView: MKAnnotationView {
         super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
         displayPriority = .required
         collisionMode = .rectangle
-        canShowCallout = true
+        canShowCallout = false
         frame = CGRect(x: 0, y: 0, width: 40, height: 40)
 //        centerOffset = CGPoint(x: 0, y: -frame.size.height/2)
         setupUI()
@@ -51,5 +70,17 @@ class CustomTestResultClusterView: MKAnnotationView {
             countLabel.text = nil
             countLabel.isHidden = true
         }
+    }
+    
+    // Used when mapView cannot zoom in and want to view results of a clustered annotation at max zoom
+    public func annotationInAlert() -> UIAlertController? {
+        guard let annotation = self.annotation as? MKClusterAnnotation else { return nil }
+        let members = annotation.memberAnnotations.map({$0 as! CustomPointAnnotation})
+        let alert = UIAlertController(title: "Clustered Results for '\(annotation.title!)'", message: "Showing results for this cluster", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Done", style: .cancel))
+        for member in members {
+            alert.addAction(UIAlertAction(title: "\(member.subtitle!)", style: .default))
+        }
+        return alert
     }
 }
