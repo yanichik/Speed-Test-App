9393398c5fff42d860bb38a9baca4fe2e37186df
Yan Brunshteyn <brunshte@gmail.com>
2024-06-18 16:52:55 -0700

    MKCoordinateSpan extension

--- SpeedTest/Extensions/MKCoordinateSpan.swift
+++ SpeedTest/Extensions/MKCoordinateSpan.swift
@@ -5,4 +5,15 @@
 //  Created by admin on 6/17/24.
 //
 
-import Foundation
+import MapKit
+
+extension MKCoordinateSpan: Equatable {
+    public static func == (lhs: MKCoordinateSpan, rhs: MKCoordinateSpan) -> Bool
+        {
+            if lhs.latitudeDelta != rhs.latitudeDelta || lhs.longitudeDelta != rhs.longitudeDelta
+            {
+                return false
+            }
+            return true
+        }
+}
