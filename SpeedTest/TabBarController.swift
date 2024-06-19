b9989b628172658d20b8210940bc494494f56fa9
Yan Brunshteyn <brunshte@gmail.com>
2024-06-19 16:09:21 -0700

    add TabBarController and set text color, add Views group

---
+++ SpeedTest/TabBarController.swift
@@ -0,0 +1,33 @@
+//
+//  TabBarController.swift
+//  SpeedTest
+//
+//  Created by admin on 6/19/24.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    @IBOutlet weak var customTabBar: UITabBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        for item in customTabBar.items! {
+            item.setTitleTextAttributes([NSAttributedString.Key.foregroundColor : UIColor.black], for: .normal)
+        }
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
